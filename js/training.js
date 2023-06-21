da26e7160bd3e4fee1a10d9beddd99ce5a8dce72
Mario Eduardo <eduardoat44@gmail.com>
2023-06-21 13:34:22 -0600

    Se ingresaron los datos en el about me

--- js/training.js
+++ js/training.js
@@ -1,44 +1,28 @@
 const urlTraining = "https://portfolio-l2h9.onrender.com/api/academic-training";
 let trainingcard = "";
 fetch(urlTraining)
-  .then((res) => res.json())
-  .then((data) => {
-    let training = data;
-    console.log(data);
-    training.forEach((element) => {
-      trainingcard += createTrainig(
-        element.title,
-        element.date,
-        element.description
-      );
+    .then((res) => res.json())
+    .then((data) => {
+        let training = data;
+        training.forEach((element) => {
+            trainingcard += createTrainig(
+                element.title,
+                element.date,
+                element.description
+            );
+        });
+        document.getElementById("olTimeline").innerHTML =
+            trainingcard || "";
     });
-    console.log(trainingcard);
-    document.getElementsByClassName("olTimeline")[0].innerHTML =
-      trainingcard +
-        `<div class="arrows">
-        <button class="arrow arrow__prev disabled" disabled>
-    <img
-      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/arrow_prev.svg"
-      alt="prev timeline arrow"
-    />
-  </button>
-        <button class="arrow arrow__next">
-    <img
-      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/arrow_next.svg"
-      alt="next timeline arrow"
-    />
-  </button>
-    </div>` || "";
-  });
 
 function createTrainig(title, date, description) {
-  let training = `
+    let training = `
         <li>
             <div>
+                <h3><strong>${title}</strong></h3><br>
                 <time>${date}</time>
-                <strong>${title}</strong><br>
                 ${description}
             </div>
         </li>`;
-  return training;
+    return training;
 }
