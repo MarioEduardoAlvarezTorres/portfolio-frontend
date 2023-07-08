e97c4d0708a5c6b47911bd12edf516a21604ebe7
Mario Eduardo <eduardoat44@gmail.com>
2023-07-07 19:52:10 -0600

    modificacion carga

--- js/training.js
+++ js/training.js
@@ -1,24 +1,24 @@
 const urlTraining = "https://portfolio-l2h9.onrender.com/api/academic-training";
 let trainingcard = "";
-document.getElementById('carga').style.display = 'flex';
+document.getElementById("carga").style.display = "flex";
 fetch(urlTraining)
-    .then((res) => res.json())
-    .then((data) => {
-        let training = data;
-        training.forEach((element) => {
-            trainingcard += createTrainig(
-                element.title,
-                element.date,
-                element.description
-            );
-        });
-        document.getElementById("olTimeline").innerHTML =
-            trainingcard + "<li></li>" || "";
-        document.getElementById('carga').style.display = 'none';
+  .then((res) => res.json())
+  .then((data) => {
+    let training = data;
+    training.forEach((element) => {
+      trainingcard += createTrainig(
+        element.title,
+        element.date,
+        element.description
+      );
     });
+    document.getElementById("olTimeline").innerHTML =
+      trainingcard + "<li></li>" || "";
+    document.getElementById("carga").style.display = "none";
+  });
 
 function createTrainig(title, date, description) {
-    let training = `
+  let training = `
         <li>
             <div>
                 <h3><strong>${title}</strong></h3><br>
@@ -26,5 +26,5 @@ function createTrainig(title, date, description) {
                 ${description}
             </div>
         </li>`;
-    return training;
+  return training;
 }
